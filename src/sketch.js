cf77106fa6802fe316f65032d9dba9abd667b59e
MJ Bingley <kn_b_y@yahoo.co.uk>
2021-04-10 18:00:23 +0100

    Added RectangularBoundary class with bounce method()

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,8 @@
 import p5 from 'p5';
 
 import Rectangle from './geometry/rectangle';
-import distanceBetweenParticles from './geometry/particle';
 import Particle from './geometry/particle';
+import RectangularBoundary from './geometry/rectangular-boundary';
 
 function drawRectangle(p5, r) {
     p5.rect(r.x, r.y, r.width, r.height);
@@ -12,33 +12,16 @@ function drawParticle(p5, p) {
     p5.circle(p.position.x, p.position.y, p.radius*2);
 }
 
-function particleBounceDetect(p, r) {
-    if (!r.isWithinRight(p.position.x, p.radius)) {
-        p.velocity.x = -p.velocity.x;
-    } else {
-        if (!r.isWithinLeft(p.position.x, p.radius)) {
-            p.velocity.x = -p.velocity.x;
-        }
-    }
-
-    if (!r.isWithinTop(p.position.y, p.radius)) {
-        p.velocity.y = -p.velocity.y;
-    } else {
-        if (!r.isWithinBottom(p.position.y, p.radius)) {
-            p.velocity.y = -p.velocity.y;
-        }
-    }
-
-}
-
 let rectangle;
 let p1, p2;
+let boundary;
 
 const sketch = (s) => {
 
     s.setup = () => {
         s.createCanvas(s.windowWidth, s.windowHeight);
         rectangle = new Rectangle(10, 10, 400, 600);
+        boundary = new RectangularBoundary(rectangle);
         let max = 4;
         p1 = new Particle(220, 300, 40, 2 * max * (Math.random() - 0.5), 2 * max * (Math.random() - 0.5));
         p2 = new Particle(170, 200, 40, 2 * max * (Math.random() - 0.5), 2 * max * (Math.random() - 0.5));
@@ -52,12 +35,13 @@ const sketch = (s) => {
 
         s.fill(126, 255, 126);
         s.noStroke();
+        
         p1.update();
-        particleBounceDetect(p1, rectangle);
+        boundary.bounce(p1);
         drawParticle(s, p1);
 
         p2.update();
-        particleBounceDetect(p2, rectangle);
+        boundary.bounce(p2);
         drawParticle(s, p2);
     };
 
