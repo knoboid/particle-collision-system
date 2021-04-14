5cdfe44f67c43314d3b66ed732af459d400ef4c1
MJ Bingley <kn_b_y@yahoo.co.uk>
2021-04-14 13:48:21 +0100

    Added particle collision manager.
    Implimented ParticleCollisionManager in sketch to handle the boundary
    collisions.

--- src/sketch.js
+++ src/sketch.js
@@ -3,6 +3,8 @@ import p5 from 'p5';
 import Rectangle from './geometry/rectangle';
 import Particle from './geometry/particle';
 import RectangularBoundary from './geometry/rectangular-boundary';
+import RectangularBoundaryCollisionDetector from './geometry/rectangular-boundary-collision-detector';
+import ParticleCollisionManager from './geometry/particle-collision-manager';
 
 function drawRectangle(p5, r) {
     p5.rect(r.x, r.y, r.width, r.height);
@@ -15,16 +17,28 @@ function drawParticle(p5, p) {
 let rectangle;
 let p1, p2;
 let boundary;
+let p1BCD, p2BCD;
+let p1PCM, p2PCM;
 
 const sketch = (s) => {
 
     s.setup = () => {
         s.createCanvas(s.windowWidth, s.windowHeight);
-        rectangle = new Rectangle(10, 10, 400, 600);
+        rectangle = new Rectangle(10, 10, 400, 350);
         boundary = new RectangularBoundary(rectangle);
-        let max = 3;
+
+        let max = 2;
         p1 = new Particle(220, 300, 60, 2 * max * (Math.random() - 0.5), 2 * max * (Math.random() - 0.5));
         p2 = new Particle(170, 200, 60, 2 * max * (Math.random() - 0.5), 2 * max * (Math.random() - 0.5));
+        
+        p1PCM = new ParticleCollisionManager(p1);
+        p2PCM = new ParticleCollisionManager(p2);
+        
+        p1BCD = new RectangularBoundaryCollisionDetector(p1, rectangle);
+        p2BCD = new RectangularBoundaryCollisionDetector(p2, rectangle);
+
+        p1PCM.addCollisionDetector(p1BCD);
+        p2PCM.addCollisionDetector(p2BCD);
     };
 
     s.draw = () => {
@@ -35,13 +49,11 @@ const sketch = (s) => {
 
         s.fill(191, 255, 127);
         s.noStroke();
-        
-        p1.update();
-        boundary.bounce(p1);
+
+        p1PCM.update(1);
         drawParticle(s, p1);
 
-        p2.update();
-        boundary.bounce(p2);
+        p2PCM.update(1);
         drawParticle(s, p2);
     };
 
