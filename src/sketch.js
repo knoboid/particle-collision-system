cba3f8404f54c62add50cb806e98b75746f4a1d5
MJ Bingley <kn_b_y@yahoo.co.uk>
2021-05-18 20:23:39 +0100

    Added coloured particles.
    Added constants that allow better control of the initial conditions.

--- src/sketch.js
+++ src/sketch.js
@@ -7,47 +7,56 @@ import ParticleSystem from './particle-collision-system/particle-system';
 import ParticlePairCollisionSystem from './particle-collision-system/particle-pair-collisions/particle-pair-collision-system';
 import BoundaryParticleCollisionSystem from './particle-collision-system/boundary-particle-collisions/boundary-particle-collision-system';
 
+const SPEED_FACTOR = 1;   // A Number greater than 0
+const SIZE_VARIABILITY = 0.7; // Vary between 0 and 0.999999999!
+const BIAS_TOWARDS_SMALLER_PARTICLES = 2; // A number between 0 and Infinity. 
+    // BIAS_TOWARDS_SMALLER_PARTICLES = 1 gives a linear fall-off.
+const SCALE = 0.04;   // A Number between 0 and 1. Particle size relative to display.
+const DENSITY = 0.25;  // A Number between 0 and 1. The tightness of particle packing.
 
 function drawRectangle(p5, r) {
     p5.rect(r.x, r.y, r.width, r.height);
 }
 
-function drawParticle(p5, p) {
-    p5.circle(p.position.x, p.position.y, p.radius*2);
+function drawParticle(p5, particle) {
+    p5.circle(particle.position.x, particle.position.y, particle.radius*2);
 }
 
 function drawParticles(p5, particleSystem) {
-    particleSystem.particles.forEach(particle => drawParticle(p5, particle));
+    particleSystem.particles.forEach(colouredParticle => {
+        p5.fill(...colouredParticle.fillArguments());
+        drawParticle(p5, colouredParticle);
+    });
 }
 
 let rectangle;
-let particleSystem;
+const particleSystem = new ParticleSystem();
 
 const sketch = (s) => {
 
     s.setup = () => {
         s.createCanvas(s.windowWidth, s.windowHeight);
 
-        const width = 400, height = 800;
+        const width = s.width, height = s.height;
         rectangle = new Rectangle(0, 0, width, height);
         const boundary = new RectangularBoundary(rectangle);
 
         const particlePairSystem = new ParticlePairCollisionSystem();
         const boundaryParticleSystem = new BoundaryParticleCollisionSystem(boundary);
 
-        particleSystem = new ParticleSystem();
         particleSystem.addCollisionSystem(particlePairSystem);
         particleSystem.addCollisionSystem(boundaryParticleSystem);
 
-        const maxSpeed = 1;
-        const step = 100;
+        const maxParticleSize = Math.min(s.windowWidth, s.windowHeight) * SCALE / 2;
+        const step = maxParticleSize * 2 / DENSITY;        
+        const maxSpeed = SPEED_FACTOR * maxParticleSize / (20 * DENSITY);
+        const minParticleSize = maxParticleSize * (1 - SIZE_VARIABILITY);
 
-        for (let i = 25; i < width; i = i + step) {
-            for (let j = 25; j < height; j += step) {
-                let r = Math.random();
-                let r2 = Math.pow(r, 5);
-                const mass = r2 * 10 + 10;
-                let particle = new Particle(i, j, mass, 2 * maxSpeed * (Math.random() - 0.5), 2 * maxSpeed * (Math.random() - 0.5));
+        for (let i = step/2; i < width - maxParticleSize; i += step) {
+            for (let j = step/2; j < height - maxParticleSize; j += step) {
+                let randomFactor = Math.pow(Math.random(), BIAS_TOWARDS_SMALLER_PARTICLES);
+                const mass = randomFactor * (maxParticleSize - minParticleSize) + minParticleSize;
+                let particle = new ColouredParticle(i, j, mass, 2 * maxSpeed * (Math.random() - 0.5), 2 * maxSpeed * (Math.random() - 0.5));
                 particleSystem.addParticle(particle);
             }
         }
@@ -58,10 +67,9 @@ const sketch = (s) => {
     s.draw = () => {
         s.background(0);
 
-        s.fill(0,63,0);
+        s.fill('#001f1f');
         drawRectangle(s, rectangle);
 
-        s.fill(191, 255, 127);
         s.noStroke();
 
         particleSystem.update(1);
@@ -79,3 +87,31 @@ const sketch = (s) => {
 };
 
 const sketchInstance = new p5(sketch);
+
+class ColouredParticle extends Particle {
+    constructor(x, y, r, xVelocity, yVelocity) {
+        super(x, y, r, xVelocity, yVelocity);
+        const yellowColour = randomColour([230, 25], [190, 50], [0, 70]);
+        const blueColour = randomColour([30, 90], [130, 90], [230, 25]);
+        const redColour = randomColour([200, 40], [30, 90], [100, 25]);
+        const greenColour = randomColour([130, 25], [230, 25], [130, 25]);
+        r = Math.random();
+        this.colour = r < 0.3 ? yellowColour : r < 0.6 ? blueColour : r < 0.9 ? redColour: greenColour;
+    }
+    
+    fillArguments() {
+        return [this.colour.red, this.colour.green, this.colour.blue];
+    }
+}
+
+function colourRange(lowerLimit,spread) {
+    return lowerLimit + Math.floor(spread * Math.random());
+}
+
+function randomColour(redRange, greenRange, blueRange) {
+    return {
+        red: colourRange(...redRange), 
+        green: colourRange(...greenRange),
+        blue: colourRange(...blueRange)
+    }; 
+}
